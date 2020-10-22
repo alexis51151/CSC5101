b4098ede6c3c4b890413f749847457f011b3293a
Alexis LE GLAUNEC <alexis.le_glaunec@telecom-sudparis.eu>
2020-10-22 17:12:26 +0200

    TP 6 on non-volatile main memory. End of question 1.e Tout fonctionne bien.

--- TP6 - NVMM/nvmm.c
+++ TP6 - NVMM/nvmm.c
@@ -7,67 +7,114 @@
 #include "nvmm.h" // All the NVMM structs
 #include "string.h"
 #include <sys/mman.h>
+#include <stdatomic.h>
+#include <pthread.h>
 
-#define DATA "test.dat"
-#define DATASIZE 128*1024*1024
+#define DATA "nvmm.dat"
+#define OUT  "out.dat"
+#define DATASIZE (128*1024*1024+4)
 
-struct nvmm* nvmm; // pointer to NVMM
-size_t       head; // where we add the entries in the log
 
-void pwb(void *p) {
-	asm volatile("clwb (%0)\n\t" : : "r"(p) : "memory");
+struct nvmm*   nvmm; // pointer to NVMM
+size_t _Atomic head; // where we add the entries in the log
+int nvmm_fd;
+
+void* thread_clean(){
+	printf("Entering cleanup\n");
+	int c = 0;
+	while(c < 1024){
+		/* Wait for an operation to read*/
+		while(nvmm->operation[nvmm->tail].committed != true){
+		}
+		printf("Valeur n° %d\n", c/5);
+		/* Propagate to the file */
+		pwrite(nvmm->operation[nvmm->tail].fd, (char*) nvmm->operation[nvmm->tail].buf, nvmm->operation[nvmm->tail].n, nvmm->operation[nvmm->tail].off);
+		fsync(nvmm->tail);
+		nvmm->operation[nvmm->tail].committed = false;
+		pfence();
+		nvmm->tail = (nvmm->tail+1)%NB_ENTRIES;
+		psync();
+		c += 5;
+	}
+	return (void*) 0;
 }
 
-void pfence() {
-	asm volatile("sfence" ::: "memory");
-}
-
-void psync() {
-	asm volatile("sfence" ::: "memory");
-}
 
 int my_pwrite(int fd, void* buf, size_t count, off_t offset){
+
 	/* Wait while the log is full */
-	/*
 	while(head+1 == nvmm->tail){
+		// Wait
+		printf("Sleeping....\n");
 	}
-	struct operation* op = malloc(sizeof(struct operation));
-	// Fill the operation structure
-	memcpy(op->buf, buf, count);
-	op->committed = true;
-	op->fd = fd;
-	op->n = count;
-	op->off = offset;
-	*/
-	return pwrite(fd, buf, count, offset);
+
+	/* Add the operation in the NVMM */
+	if(nvmm->operation[head].committed == false){
+		memcpy(nvmm->operation[head].buf, buf, count);
+		pwb(&nvmm->operation[head].buf);
+		nvmm->operation[head].n = count;
+		pwb(&nvmm->operation[head].n);
+		nvmm->operation[head].fd = fd;
+		pwb(&nvmm->operation[head].fd);
+		nvmm->operation[head].off = offset;
+		pwb(&nvmm->operation[head].off);
+		pfence();
+		nvmm->operation[head].committed = true;
+		pwb(&nvmm->operation[head].committed);
+		psync();
+	}
+	printf("Valeur après pwrite %s \n", nvmm->operation[head].buf);
+	head = (head+1)%NB_ENTRIES;
+	printf("Head value : %ld\n", head);
+	return EXIT_SUCCESS;
 }
 
+
 int main(int argc, char** argv){
-	int fd = open(DATA, O_CREAT | O_TRUNC | O_RDWR);
-	if(fd < 0){
+
+	/* Initializing head */
+	head = 0;
+
+	/* Creating the NVMM file */
+	nvmm_fd = open(DATA, O_CREAT | O_RDWR | O_TRUNC, 644);
+	if(nvmm_fd < 0){
 		printf("open : Could not access file %s", DATA);
 		return EXIT_FAILURE;
 	}
-	if( ftruncate(fd, DATASIZE) < 0){
+	if( ftruncate(nvmm_fd, sizeof(struct nvmm)) < 0){
 		printf("truncate : Could not truncate file %s", DATA);
 		return EXIT_FAILURE;
 	}
-	int* addr = mmap(NULL, DATASIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 128*1024);
-	if(addr == (void*) -1){
+	nvmm = mmap(NULL, sizeof(struct nvmm), PROT_READ | PROT_WRITE, MAP_SHARED, nvmm_fd, 0);
+	if(nvmm == (void*) -1){
 		printf("mmap failed");
 		return EXIT_FAILURE;
 	}
-	addr[0]++;
+	nvmm->tail = head;
+	pwb(&nvmm->tail);
+	psync();
+	pthread_t cleanup;
+	pthread_create(&cleanup, NULL, thread_clean, NULL);
 
-	/*
-	char msg[10] = "hello";
-	/* Write hello in the DATA file 1024 times */
-	/*
+
+	/* Open the file to write to */
+	int out_fd = open(OUT, O_CREAT | O_RDWR | O_TRUNC, 600);
+	if(out_fd < 0){
+		printf("open : Could not access file %s", DATA);
+		return EXIT_FAILURE;
+	}
+
+	char msg[5] = "hello";
+
 	for(int off = 0; off < 1024;off+=5){
-		if(my_pwrite(fd, msg, 10, off) < 0 ){
+		if(my_pwrite(out_fd, msg,  sizeof(msg), off) < 0){
 			return EXIT_FAILURE;
 		}
 	}
-	*/
+
+	pthread_join(cleanup, NULL);
+	munmap(nvmm, DATASIZE);
+	close(nvmm_fd);
+	close(out_fd);
 	return EXIT_SUCCESS;
 }
--- TP6 - NVMM/nvmm.h
+++ TP6 - NVMM/nvmm.h
@@ -2,16 +2,39 @@
 // Created by alexis51151 on 22/10/2020.
 //
 
-#ifndef TP6_NVMM_NVMM_H
-#define TP6_NVMM_NVMM_H
+#ifndef _NVMM_H_
+#define _NVMM_H_
 
-#endif //TP6_NVMM_NVMM_H
+// note: compile with the option -march=native
+
+#if defined(__CLWB__)
+//#pragma message "Using clwb as pwb implementation"
+static void pwb(void *p) { asm volatile("clwb (%0)\n\t" : : "r"(p) : "memory"); }
+static void pfence()     { asm volatile("sfence" ::: "memory"); }
+static void psync()      { asm volatile("sfence" ::: "memory"); }
+#elif defined(__CLFLUSHOPT__)
+//#pragma message "Using clflushopt as pwb implementation"
+static void pwb(void *p) { asm volatile("clflushopt (%0)\n\t" : : "r"(p) : "memory"); }
+static void pfence()     { asm volatile("sfence" ::: "memory"); }
+static void psync()      { asm volatile("sfence" ::: "memory"); }
+#else
+//#pragma message "Using default clflush as pwb implementation, try to compile with -march=native"
+static void pwb(void *p) { asm volatile("clflush (%0)\n\t" : : "r"(p) : "memory"); }
+static void pfence()     { asm volatile("" ::: "memory"); }
+static void psync()      { asm volatile("" ::: "memory"); }
+#endif
+
+#endif
 
 #include <stdbool.h>
 #include <sys/types.h>
 
-#define NB_ENTRIES 1024*1024
+#define NB_ENTRIES (1024*1024)
 #define CACHE_LINE 64
+
+// note: compile with the option -march=native
+
+
 
 struct operation {
 	union {                // first cache line
