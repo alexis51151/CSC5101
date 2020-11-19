13a83ecc9faf0ab1758e9a4ff2efbf0ee3da598b
Alexis LE GLAUNEC <alexis.le_glaunec@telecom-sudparis.eu>
2020-11-19 10:56:35 +0100

    Question 1.d

--- CF/synchro.c
+++ CF/synchro.c
@@ -24,6 +24,7 @@ struct queue {
 // global variables
 struct queue jobs;
 pthread_mutex_t lock;
+pthread_cond_t cond;
 
 // FIFO initialization
 void init() {
@@ -37,28 +38,47 @@ void master(){
 	while(1){
 		memset(cmd, 0, 256);
 		read(0, cmd, 256);
-		if(strncmp(cmd, "quit",4) == 0){
-			printf("Quitting master function\n");
-			return;
-		}
 		// Setting up the new command
 		struct node* node = malloc(sizeof(struct node));
-		printf("cmd = %s\n", cmd);
 		memcpy(node->cmd, cmd, 256);
 
 		// Add it to the jobs queue
 		pthread_mutex_lock(&lock);
 		jobs.tail->next = node;
 		jobs.tail = node;
+		// tells the slaves that we've added a new command
+		pthread_cond_signal(&cond);
 		pthread_mutex_unlock(&lock);
-
+		// we put it here because it's still a command to be considered by slaves
+		if(strncmp(cmd, "quit",4) == 0){
+			printf("Quitting master function\n");
+			return;
+		}
 	}
 }
 
 
 void* slave() {
 	printf("Slave called\n");
-	pthread_exit(0);
+	char cmd[256];
+	while(1) {
+		memset(cmd, 0, 256);
+		pthread_mutex_lock(&lock);
+		// wait until master wakes it up if the list is empty
+		while(!jobs.head->next) {
+			pthread_cond_wait(&cond, &lock);
+		}
+		// Remove the last command (at head)
+		memcpy(cmd, jobs.head->next->cmd, 256);
+		jobs.head = jobs.head->next;
+		pthread_mutex_unlock(&lock);
+		if(strncmp(cmd, "quit",4) == 0){
+			printf("Quitting slave function\n");
+			pthread_exit(0);
+		}
+		printf("System call on %s\n", cmd);
+		system(cmd);
+	}
 }
 
 int main(int argc, char** argv){
