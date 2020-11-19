9e637abe7e6c59c7db4460265aa9e892052ce047
Alexis LE GLAUNEC <alexis.le_glaunec@telecom-sudparis.eu>
2020-11-19 10:41:59 +0100

    Question 1.c

--- CF/synchro.c
+++ CF/synchro.c
@@ -6,6 +6,8 @@
 #include <pthread.h>
 #include <stdint.h>
 #include <stdatomic.h>
+#include <unistd.h>
+#include <string.h>
 
 
 struct node {
@@ -15,17 +17,42 @@ struct node {
 
 // FIFO queue
 struct queue {
-	struct node head;
-	struct node tail;
+	struct node* head;
+	struct node* tail;
 };
 
 // global variables
 struct queue jobs;
+pthread_mutex_t lock;
 
+// FIFO initialization
+void init() {
+	jobs.head = jobs.tail = malloc(sizeof(struct node));  // fake node
+	jobs.head->next = 0;
+}
 
-void* master(){
+void master(){
 	printf("Master called\n");
-	pthread_exit(0);
+	char cmd[256];
+	while(1){
+		memset(cmd, 0, 256);
+		read(0, cmd, 256);
+		if(strncmp(cmd, "quit",4) == 0){
+			printf("Quitting master function\n");
+			return;
+		}
+		// Setting up the new command
+		struct node* node = malloc(sizeof(struct node));
+		printf("cmd = %s\n", cmd);
+		memcpy(node->cmd, cmd, 256);
+
+		// Add it to the jobs queue
+		pthread_mutex_lock(&lock);
+		jobs.tail->next = node;
+		jobs.tail = node;
+		pthread_mutex_unlock(&lock);
+
+	}
 }
 
 
@@ -41,6 +68,8 @@ int main(int argc, char** argv){
 	}
 	int n = atoi(argv[1]);
 	printf("n = %d\n", n);
+
+	init();
 
 	pthread_t threads[n];
 	for(int i = 0; i < n; i++){
